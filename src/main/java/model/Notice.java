515418bf81e925431af9552f90168fed2c4c469d
SSOsh <53353714+SSOsh@users.noreply.github.com>
2020-11-12 13:42:03 +0900

    20201112
    save

--- src/main/java/model/Notice.java
+++ src/main/java/model/Notice.java
@@ -1,11 +1,21 @@
 package model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class Notice {
+    @Getter
+    @Setter
     private int noticeID;
+    @Getter
+    @Setter
     private String title;
+    @Getter
+    @Setter
     private String contents;
+    @Getter
+    @Setter
     private String date;
 }
